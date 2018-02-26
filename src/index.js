c759a43c74bd6e4cde4a084d11606e0bc45948a1
Lenovo <nikaz@tut.by>
2018-02-27 00:43:03 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
+
+let stack=[];
+
+for (let i=0; i<str.length; i++) {
+    console.log (`i=`+i);
+    for (let j=0; j<bracketsConfig.length; j++) {
+
+
+        if (str[i] === bracketsConfig[j][0]) {stack.push(str[i]); }
+
+
+       // console.log (stack);
+
+        if (str[i] === bracketsConfig[j][1]) {
+            console.log (`j=`+j);
+
+            let a = stack.pop(str[i]);
+            console.log (stack);
+
+            if (a === undefined){  console.log ("undef"); return false;}
+
+            if (a != bracketsConfig[j][0]){  console.log ("expected " +bracketsConfig[j][1] + "found " + a ); return false;}
+
+
+        }
+    }
+
+    }
+    if (stack.length!=0 )return false;
+    return true;
+
+
+
+
+
+
   // your solution
 }
